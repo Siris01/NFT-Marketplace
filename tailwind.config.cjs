a1207f1ac0a4d1c1abdccd2f6d7a5edd4f245f3a
Siris <vincentcmv@gmail.com>
2022-05-23 19:15:53 +0530

    feat: inital commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	content: ['./src/routes/*.ts', './src/views/pages/*.ejs', './src/views/partials/*.ejs'],
+	theme: {
+		minWidth: {
+			'1/3': '33vw',
+			'1/4': '25vw'
+		}
+	}
+};
